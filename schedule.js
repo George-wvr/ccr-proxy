6d7e2e3eaa3b336b377e799816d8386a5c44aacc
George <162292571+George-wvr@users.noreply.github.com>
2025-05-18 20:19:20 +0100

    Update Schedule
    Update Schedule with remainder of Sunday and whole of Monday

--- schedule.js
+++ schedule.js
@@ -1,25 +1,116 @@
 //Schedule for CCR 104.4FM
 
 export const schedule = [
+  //Monday
   {
     day: 'Monday',
-    start: '09:00',
-    end: '12:00',
-    title: 'The Breakfast Show',
-    presenters: ['Alice Johnson']
+    start: '06:00',
+    end: '07:30',
+    title: 'Early Breakfast',
+    presenters: ['Dave Daniels']
   },
+  {
+    day: 'Monday',
+    start: '07:30',
+    end: '10:00',
+    title: 'Good Morning Chelmsford',
+    presenters: ['Michelle Durrant']
+  },
+
+  {
+    day: 'Monday',
+    start: '10:00',
+    end: '12:00',
+    title: 'The Retro Chart Show',
+    presenters: ['Mark Willis']
+  },
+
   {
     day: 'Monday',
     start: '12:00',
-    end: '15:00',
-    title: 'Midday Mix',
-    presenters: ['Ben Clarke', 'Sophie Lee']
+    end: '14:00',
+    title: 'Lunchtime',
+    presenters: ['Amy Boyd']
   },
+
+  {
+    day: 'Monday',
+    start: '14:00',
+    end: '15:00',
+    title: 'The Will Show',
+    presenters: ['Will McKenzie']
+  },
+
+  {
+    day: 'Monday',
+    start: '15:00',
+    end: '17:00',
+    title: 'Ignition',
+    presenters: ['Ben Kerns']
+  },
+
+  {
+    day: 'Monday',
+    start: '17:00',
+    end: '19:00',
+    title: 'Drive at Five',
+    presenters: ['Richard Taylor']
+  },
+
+  {
+    day: 'Monday',
+    start: '19:00',
+    end: '20:00',
+    title: 'The Alternative Hour',
+    presenters: ['Doug Beasley']
+  },
+
+  {
+    day: 'Monday',
+    start: '20:00',
+    end: '22:00',
+    title: 'City Talk',
+    presenters: ['Nita Jhummu']
+  },
+
+  {
+    day: 'Monday',
+    start: '23:00',
+    end: '00:00',
+    title: 'Monday Night Mayhem',
+    presenters: ['Luke Woolley','Tobias']
+  },
+  
+  //Sunday
   {
     day: 'Sunday',
     start: '18:00',
     end: '20:00',
     title: 'Sunday Selection',
     presenters: ['Andrew Alton Reid']
+  },
+
+  {
+    day:'Sunday',
+    start: '20:00',
+    end: '21:00',
+    title: 'Smooth Classics',
+    presenters: ['Deborah Aloba']
+  },
+
+  {
+    day:'Sunday',
+    start: '21:00',
+    end: '22:40',
+    title: 'The Chelmsford and District Talking Newspaper',
+    presenters: ['The Talking Newspaper']
+  },
+
+  {
+    day:'Sunday',
+    start: '22:40',
+    end: '00:00',
+    title: 'Amber in the Evening',
+    presenters: ['Amber']
   },
 ];
